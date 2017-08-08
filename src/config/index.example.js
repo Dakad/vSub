73cd9b84d929135895c8f7bb1b9478fb6e3739c4
Dakad <akad@live.be>
2017-08-08 17:54:30 +0000

    Replace Config package with simple homemade config js

---
+++ src/config/index.example.js
@@ -0,0 +1,15 @@
+
+import defaultConfig from './default.json'
+
+
+export default Object.assign(defaultConfig, {
+    "movieDB" : {
+        "apiKey" : "ilVa5ODsbIx697d5Rpd@Da3ocOBXv3"
+    },
+    "openSubtitle" : {
+        "useragent": "UserAgent",
+        "username": "qwertz",
+        "password": "qwertz123",
+    }
+})
+
