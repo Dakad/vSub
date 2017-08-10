629d0888d40a4aef17ae7b7757ac4684dd476db3
Dakad <akad@live.be>
2017-08-10 08:29:11 +0000

    Dynamic insert of global components

--- src/main.js
+++ src/main.js
@@ -4,18 +4,26 @@ import Vue2Filters from 'vue2-filters'
 import VueRouter from 'vue-router'
 
 
-
 import routes from './routes/'
 import store from './store/'
 import App from './App.vue'
 
+import components from './components/global';
 
 
 Vue.use(Vue2Filters)
 Vue.use(VueRouter)
 
 
+/**
+ * Register Global Components
+ *
+ */
+Object.keys(components)
+      .forEach(tag => Vue.component(tag, components[tag]));
 
+
+// Instanciate the app and mount it to the DOM (#app)
 new Vue({ // eslint-disable-line no-new
   el: '#app',
   store,
