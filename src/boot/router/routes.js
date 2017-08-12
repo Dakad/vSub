a3987d998e60deb002e5f8b6254acc3fc22dd6ad
Dakad <akad@live.be>
2017-08-12 01:44:33 +0000

    Add Route to Summary Page

--- src/boot/router/routes.js
+++ src/boot/router/routes.js
@@ -1,10 +1,11 @@
 
 import Dropzone from '../../pages/Dropzone.vue'
-import Summary from '../../pages/summary/Summary.vue'
+import Summary from '../../pages/Summary.vue'
 
 export default [
   { name: 'home', path: '/', redirect: '/drop'},
   { name: 'drop', path: '/drop', component: Dropzone },
-  { name: 'summary', path: '/summary', alias: '/dropped',component: Summary },
+  { name: 'dropped', path: '/dropped', redirect: '/summary'},
+  { name: 'summary', path: '/summary',component: Summary },
   
 ]
