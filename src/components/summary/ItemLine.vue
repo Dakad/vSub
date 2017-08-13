3552b84301b00cacc8496c765a4e38b6b52502da
Dakad <akad@live.be>
2017-08-13 08:46:59 +0000

    Emit $event on actions btn click

--- src/components/summary/ItemLine.vue
+++ src/components/summary/ItemLine.vue
@@ -2,31 +2,31 @@
   <li :class="['item', {checked:item.hasSub}]">
     <router-link class="vid"  :to="'/summary/'+item.hash" :title="'Details on '+item.name">
       {{item.name}}&nbsp;
-      <span v-if="item.season || item.episode">
-        <span v-if="item.season">S{{item.season}}E{{item.episode}}</span>
+      <span v-if="item.kind == 'movie'">({{item.year}})</span>
+      <span v-else>S{{item.season}}E{{item.episode}}</span>
       </span>
-      <span v-else>({{item.year}})</span>
     </router-link>
     <span class="actions">
-      <span v-if="item.hasSub" class="btn cast fa fa-rss"></span>
-      <span v-if="item.hasSub" class="btn play fa fa-play"></span>
+      <span v-if="item.hasSub" class="btn cast fa fa-rss" @click="$emit('cast', item.hash)"></span>
+      <span v-if="item.hasSub" class="btn play fa fa-play" @click="$emit('play', item.hash)"></span>
       <router-link class="btn info fa fa-info-circle" 
                    v-if="item.hasSub"
                    :to="'/summary/'+item.hash" :title="'Details on '+item.name">
       </router-link>
-      <span v-if="!item.hasSub" class="btn sub fa fa-file-text"></span>
+      <span v-if="!item.hasSub" class="btn sub fa fa-file-text" @click="$emit('getSubtitle', item.hash)"></span>
     </span>
   </li>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
   
   export default {
     name: 'item-line',
     props: ['item'],
     
     computed : {
-      
+      // ... mapGetters(['hasSub'])
     }
   }
 </script>
