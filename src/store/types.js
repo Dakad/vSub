0159a11d5f6118994a53dd7e7d9511d07645c45e
Dakad <akad@live.be>
2017-08-13 19:39:44 +0000

    Rename types for fetched subtitles
    Add mutations types for downloadeds subs

--- src/store/types.js
+++ src/store/types.js
@@ -1,52 +1,57 @@
 /**
  * ACTIONS
  */
-export const HANDLE_FILES       = 'handleFiles'
-export const FETCH_SUB          = 'fetchSubtitles'
+export const HANDLE_FILES          = 'handleFiles'
+export const FETCH_SUB             = 'fetchSubtitles'
  
 
 /**
  * ERRORS
  */
-export const ADD_ERROR          = 'ADD_ERROR'
-export const REMOVE_ERROR       = 'REMOVE_ERROR'
+export const ADD_ERROR             = 'ADD_ERROR'
+export const REMOVE_ERROR          = 'REMOVE_ERROR'
 
 
 /**
  * LOADER
  */
-export const START_LOADING      = 'START_LOADING'
-export const END_LOADING        = 'END_LOADING'
+export const START_LOADING         = 'START_LOADING'
+export const END_LOADING           = 'END_LOADING'
 
 
 /**
  * DRAG & DROP
  */
-export const DRAG_ENTER         = 'DRAG_ENTER'
-export const DRAG_LEAVE         = 'DRAG_LEAVE'
+export const DRAG_ENTER            = 'DRAG_ENTER'
+export const DRAG_LEAVE            = 'DRAG_LEAVE'
 
 
 
 /**
  * SUMMARRY
  */
-export const GET_DETAILS        = 'GET_DETAILS'
+export const GET_DETAILS           = 'GET_DETAILS'
 
 
 /**
  * DROPPED
  */
-export const DROPPED_Q          = 'DROPPED_Q'
-export const SHIFT_DROPPED_Q    = 'SHIFT_DROPPED_Q'
-export const RESET_DROPPED_Q    = 'RESET_DROPPED_Q'
+export const DROPPED_Q             = 'DROPPED_Q'
+export const SHIFT_DROPPED_Q       = 'SHIFT_DROPPED_Q'
+export const RESET_DROPPED_Q       = 'RESET_DROPPED_Q'
     // Summary part
-export const ADD_SUMMARY        = 'ADD_SUMMARY'
-export const RESET_SUMMARY_LIST = 'RESET_SUMMARY_LIST'
+export const ADD_SUMMARY           = 'ADD_SUMMARY'
+export const RESET_SUMMARY_LIST    = 'RESET_SUMMARY_LIST'
 
 
 /**
  * SUBTITLES
  */
-export const GET_SUB_FOR        = 'GET_SUB_FOR_VID'
-export const ADD_SUB_FOR        = 'ADD_SUB_FOR_VID'
-export const RESET_SUB_FOR      = 'RESET_SUB_FOR_VID'
+export const GET_FETCHED_SUB_FOR   = 'GET_FETCHED_SUB_FOR_VID'
+export const ADD_FETCHED_SUB_FOR   = 'ADD_FETCHED_SUB_FOR'
+export const RESET_FETCHED_SUB_FOR = 'RESET_FETCHED_SUB_FOR_VID'
+
+export const GET_DOWN_SUB_FOR      = 'GET_FECTHED_SUB_FOR_VID'
+export const ADD_DOWN_SUB_FOR      = 'ADD_DOWN_SUB_FOR'
+export const RESET_DOWN_SUB_FOR    = 'RESET_DOWN_SUB_FOR_VID'
+
