0dbb64a410169e2f2a5c2513226006c1201f928f
Dakad <akad@live.be>
2018-01-20 02:29:24 +0100

    Start gulp task dev on watch mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
 'use strict';
- 
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 const input = './styles/sass/**/*.scss';
-const output ='./dist';
+const output = './dist';
 const sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'compressed'
+    errLogToConsole: true,
+    outputStyle: 'compressed'
 };
 
 
 gulp.task('default', ['dev', 'prod']);
- 
 
-gulp.task('prod', function () {
-  return gulp.src(input)
-    .pipe(sass(sassOptions)
-      .on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(output));
+
+gulp.task('prod', function() {
+    return gulp.src(input)
+        .pipe(sass(sassOptions)
+            .on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(output));
 });
 
-gulp.task('dev', function () {
-  return gulp.src(input)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(output));
+gulp.task('dev', function() {
+    return gulp.src(input)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(output));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(input, ['dev'])
-      .on('change', e => console.log(`File ${e.path} was ${e.type}, running tasks...`))
+gulp.task('watch', function() {
+    gulp.start('dev');
+    gulp.watch(input, ['dev'])
+        .on('change', e => console.log(`File ${e.path} was ${e.type}, running tasks...`))
 });
